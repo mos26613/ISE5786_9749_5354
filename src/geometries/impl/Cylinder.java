bd966cf006c912f0421c3651607853a5ccefebe7
Steiner Aron <aron.steiner@icloud.com>
2026-03-12 22:15:13 +0200

    Add Cylinder class to represent a cylinder geometry with a circular base and height

---
+++ src/geometries/impl/Cylinder.java
@@ -0,0 +1,22 @@
+package geometries.impl;
+
+import primitives.Ray;
+
+/** * Represents a cylinder geometry, which is defined by a circular base and a height.
+ * The cylinder is a three-dimensional shape that extends the properties of a tube by adding height.
+ */
+public class Cylinder extends Tube {
+    /** Height of the cylinder */
+    private final double height;
+
+    /**
+     * Constructs a cylinder with the given radius, axis, and height.
+     * @param radius Radius of the cylinder's base
+     * @param axis Axis of the cylinder
+     * @param height Height of the cylinder
+     */
+    public Cylinder(double radius, Ray axis, double height) {
+        super(radius, axis);
+        this.height = height;
+    }
+}
