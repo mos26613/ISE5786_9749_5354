95780608f6b591e6bd2543b306e9d0c425c094f9
Steiner Aron <aron.steiner@icloud.com>
2026-03-12 21:52:28 +0200

    Add Triangle class to represent a triangle geometry with specified vertices

---
+++ src/geometries/impl/Triangle.java
@@ -0,0 +1,19 @@
+package geometries.impl;
+
+import primitives.Point;
+
+/**
+ * Represents a triangle geometry.
+ */
+public class Triangle extends Polygon {
+    /**
+     * Constructs a triangle with the given vertices.
+     *
+     * @param p1 The first vertex of the triangle.
+     * @param p2 The second vertex of the triangle.
+     * @param p3 The third vertex of the triangle.
+     */
+    public Triangle(Point p1, Point p2, Point p3) {
+        super(p1, p2, p3);
+    }
+}
