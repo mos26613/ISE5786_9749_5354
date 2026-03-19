283de90271c9367b7a3c45035e68376044689f21
Steiner Aron <aron.steiner@icloud.com>
2026-03-19 22:53:54 +0200

    Refactor import statement in TriangleTest to use assertEquals explicitly

--- unittests/geometries/impl/TriangleTest.java
+++ unittests/geometries/impl/TriangleTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import primitives.Point;
 import primitives.Vector;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /** Test class for {@link Triangle} class */
 class TriangleTest {
