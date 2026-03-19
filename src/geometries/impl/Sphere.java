44eb2b91720a9ac4366e1275ecb49064a5766d45
Steiner Aron <aron.steiner@icloud.com>
2026-03-19 22:53:46 +0200

    Implement getNormal method in Sphere class and add SphereTest class with unit tests

--- src/geometries/impl/Sphere.java
+++ src/geometries/impl/Sphere.java
@@ -33,6 +33,6 @@ public final class Sphere extends RadialGeometry {
 
     @Override
     public Vector getNormal(Point point) {
-        return null;
+        return point.subtract(_center).normalize();
     }
 }
---
+++ unittests/geometries/impl/SphereTest.java
@@ -0,0 +1,30 @@
+package geometries.impl;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import primitives.Point;
+import primitives.Vector;
+
+/** Test class for {@link Sphere} class */
+class SphereTest {
+    /** Delta for accuracy when comparing double values */
+    private static final double DELTA = 1e-6;
+
+    /** Default constructor for SphereTest to satisfy Javadoc tool */
+    public SphereTest() {}
+
+    /** Test method for {@link Sphere#Sphere(Point, double)} */
+    @Test
+    void testGetNormal() {
+        Point center = Point.ZERO;
+        Point point = new Point(1, 0, 0);
+        Sphere sphere = new Sphere(center, 1);
+        Vector actualNormal = sphere.getNormal(point);
+
+            // ============ Equivalence Partitions Tests ==============
+
+        // TC01 checks regular case
+        assertEquals(point.subtract(center), actualNormal, "Normal vector is not correct.");
+    }
+}
