4510190403d179c1c2f23f0b60844b6685b502dc
Moshe Grossman <modotyh@gmail.com>
2026-03-29 19:23:38 +0300

    Add Geometries class and corresponding tests for intersection functionality

---
+++ src/geometries/impl/Geometries.java
@@ -0,0 +1,51 @@
+package geometries.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import geometries.api.Intersectable;
+import primitives.Point;
+import primitives.Ray;
+
+/**
+ * Represents a collection of geometries that can be intersected by rays
+ */
+public class Geometries extends Intersectable {
+
+    /**
+     * List of geometries in the collection
+     */
+    private final List<Intersectable> _geometries = new ArrayList<>();
+
+    /**
+     * Constructs a new Geometries collection with the given geometries.
+     * @param geometries The geometries to add to the collection
+     */
+    public Geometries(Intersectable... geometries) {
+        add(geometries);
+    }
+
+    /**
+     * Adds the given geometries to the collection.
+     * @param geometries The geometries to add to the collection
+     */
+    public void add(Intersectable... geometries) {
+        _geometries.addAll(List.of(geometries));
+    }
+
+    @Override
+    public List<Point> findIntersections(Ray ray) {
+        List<Point> allIntersections = null; // the final result
+        List<Point> intersections;
+
+        for (Intersectable geometry : _geometries) {
+            intersections = geometry.findIntersections(ray); // delegate and get all intersection points
+            if (intersections != null) { // no intersection points, skip to the next geometry
+                if  (allIntersections == null) // not initialized yet, initialize with the first geometry's intersection points
+                    allIntersections = new ArrayList<>();
+                allIntersections.addAll(intersections); // add the current geometry's intersection points to the final result
+            }
+        }
+        return allIntersections;
+    }
+}
---
+++ unittests/geometries/impl/GeometriesTests.java
@@ -0,0 +1,59 @@
+package geometries.impl;
+
+import org.junit.jupiter.api.Test;
+import primitives.Point;
+import primitives.Ray;
+import primitives.Vector;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit tests for class {@link Geometries}
+ */
+class GeometriesTests {
+
+    /**
+     * Default constructor to satisfy JavaDoc generator
+     */
+    public GeometriesTests() {}
+
+    /**
+     * Test method for {@link Geometries#findIntersections(Ray)}.
+     */
+    @Test
+    void testFindIntersections() {
+
+        Point P00N2 = new Point(0, 0, -2);
+        String errCount = "Wrong number of intersection points";
+        String errEmpty = "Expected no intersection points";
+
+        Plane plane = new Plane(Point.ZERO, new Vector(0, 0, 1));
+        Sphere sphere = new Sphere(Point.ZERO, 1);
+        Triangle triangle =
+                new Triangle(new Point(0, 0, 2), new Point(1, 0, 2), new Point(0, 1, 2));
+
+        Geometries geometries = new Geometries(plane, sphere, triangle);
+        Ray ray;
+
+
+        // ===== Equivalence Partitions Tests =====
+
+        // EP01: Ray intersects some but not all and more than one geometry (3 points)
+        ray = new Ray(P00N2, new Vector(0, 0, 1));
+        assertEquals(3, geometries.findIntersections(ray).size(), errCount);
+
+        // =====  Boundary Values Tests =====
+
+        // BV01: Ray doesn't intersect any geometry (0 points)
+        ray = new Ray(P00N2, new Vector(0, 1, 0));
+        assertNull(geometries.findIntersections(ray), errEmpty);
+
+        // BV02: Ray intersects exactly one geometry (1 point)
+        ray = new Ray(P00N2, new Vector(5, 5, 5));
+        assertEquals(1, geometries.findIntersections(ray).size(), errCount);
+
+        // BV03: Ray intersects all geometries (4 points)
+        ray = new Ray(P00N2, new Vector(1, 1, 12));
+        assertEquals(4, geometries.findIntersections(ray).size(), errCount);
+    }
+}
