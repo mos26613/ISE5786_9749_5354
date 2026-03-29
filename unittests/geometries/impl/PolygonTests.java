518efb9c9170ea082d638164452e7aa3824455d7
Moshe Grossman <modotyh@gmail.com>
2026-03-29 19:24:41 +0300

    Refactor PolygonTests by removing unused imports for improved code clarity

--- unittests/geometries/impl/PolygonTests.java
+++ unittests/geometries/impl/PolygonTests.java
@@ -1,16 +1,13 @@
 package geometries.impl;
 
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
 import primitives.Point;
-import primitives.Ray;
 import primitives.Vector;
 
 /**
